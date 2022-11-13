2eba1f2dfc5c92bfcc8707d7d5fc2f006bdab092
IlovingCode <ncthien93@gmail.com>
2022-11-14 00:59:55 +0700

    - reduce shader file size

--- assets/buildMaterials.sh
+++ assets/buildMaterials.sh
@@ -1,9 +1,9 @@
-# ./matc --api all -f header -o "../HelloCocoaPods/bakedColor.inc" "./bakedColor.mat"
+./matc -p mobile -a metal -f header -o "../HelloCocoaPods/bakedColor.inc" "./bakedColor.mat"
 
 # ./mipgen -f ktx2 --compression=uastc -m 1 spr_BottleCap_strip2.png spr_BottleCap_strip2.ktx2
 
-./mipgen -f ktx2 --compression=uastc -m 1 tiny.png tiny.ktx2
-./mipgen -f ktx2 --compression=uastc -m 1 red.png red.ktx2
+# ./mipgen -f ktx2 --compression=uastc -m 1 tiny.png tiny.ktx2
+# ./mipgen -f ktx2 --compression=uastc -m 1 red.png red.ktx2
 
-./mipgen -f ktx2 --compression=uastc -m 1 progress_fill.png progress_fill.ktx2
-./mipgen -f ktx2 --compression=uastc -m 1 progress_bg.png progress_bg.ktx2
+# ./mipgen -f ktx2 --compression=uastc -m 1 progress_fill.png progress_fill.ktx2
+# ./mipgen -f ktx2 --compression=uastc -m 1 progress_bg.png progress_bg.ktx2
